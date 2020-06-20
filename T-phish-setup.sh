bbbcd939854d9e8faee779f5a9613942d0897149
IT-Tejas <65008967+IT-Tejas@users.noreply.github.com>
2020-06-21 05:16:22 +0530

    Update T-phish-setup.sh

--- T-phish-setup.sh
+++ T-phish-setup.sh
@@ -5,6 +5,8 @@ cd $home
 git clone https://github.com/Stephin-Franklin/T-Phish
 cd T-Phish
 unzip T-Phish.zip
+cd $home
+rm -rf T-phish-setup
 cd T-Phish
 chmod +x start.sh
 figlet Please switch on hotspot for getting link
